92c2861bd9ef3fba099f396c2760a917a6a777b7
Ayush Narayan <ayush.perla@gmail.com>
2026-03-01 01:10:25 +0530

    fix: change login redirect parameter to ?role=

--- app/actions/auth.ts
+++ app/actions/auth.ts
@@ -29,7 +29,7 @@ export async function login(formData: FormData) {
     }
 
     revalidatePath('/', 'layout')
-    redirect(`/dashboard?type=${role}`)
+    redirect(`/dashboard?role=${role}`)
 }
 
 export async function signup(formData: FormData) {
